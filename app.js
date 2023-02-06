ff0bf24807b396c9128167f5829e6e8d5e6dfd6c
KiranR1605 <kiranr1605@gmail.com>
2023-02-06 15:42:45 +0530

    Level-5 Authentication
    Using passport, passport-local, passpot-local-mongoose and express-session packages for authentication

---
+++ app.js
@@ -0,0 +1,107 @@
+require('dotenv').config()
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
+
+const app = express();
+
+app.use(express.static("public"));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(session({
+    secret: 'this is a secret.',
+    resave: false,
+    saveUninitialized: true
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+mongoose.set('strictQuery', true);
+
+mongoose.connect("mongodb://127.0.0.1:27017/userDB");
+
+const userSchema = new mongoose.Schema({
+    email: String,
+    password: String
+});
+
+userSchema.plugin(passportLocalMongoose);
+
+const User = new mongoose.model("User", userSchema);
+
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.get("/", function (req, res) {
+    res.render("home");
+});
+
+app.route("/login")
+
+    .get(function (req, res) {
+        res.render("login");
+    })
+
+    .post(function (req, res) {
+        const user = new User({
+            username: req.body.username,
+            password: req.body.password
+        });
+
+        req.login(user, function (err) {
+            if (err) {
+                console.log(err);
+            } else {
+                passport.authenticate("local")(req, res, function () {
+                    res.redirect("/secrets");
+                });
+            }
+        });
+    });
+
+app.get("/logout", function(req, res){
+    req.logout(function(err){
+        if(err){
+            console.log(err);
+        }
+    });
+    res.redirect("/");
+});
+
+app.route("/register")
+
+    .get(function (req, res) {
+        res.render("register");
+    })
+
+    .post(function (req, res) {
+        User.register({ username: req.body.username }, req.body.password, function (err, user) {
+            if (err) {
+                res.redirect("/register");
+            } else {
+                passport.authenticate("local")(req, res, function () {
+                    res.redirect("/secrets");
+                });
+            }
+        });
+    });
+
+app.get("/secrets", function (req, res) {
+    if (req.isAuthenticated()) {
+        res.render("secrets");
+    } else {
+        res.redirect("/login");
+    }
+});
+
+app.listen(3000, function () {
+    console.log("Server started on port 3000");
+});
